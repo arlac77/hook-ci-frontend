7138968462986e705b2460150df2ca7cc3f23cd9
Markus Felten <Markus.Felten@gmx.de>
2019-05-04 18:51:51 +0200

    chore: dist -> build

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
 import copy from "rollup-plugin-copy";
 
 const production = !process.env.ROLLUP_WATCH;
-const dist = 'dist';
+const dist = 'build';
 
 const config = {
   input: "src/main.mjs",
@@ -22,19 +20,5 @@ const config = {
     })
   ]
 };
-
-if(!production) {
-config.plugins.push(
-  serve({
-    contentBase: `./${dist}/`,
-    open: true
-  }),
-  livereload({
-    watch: dist,
-    verbose: true,
-    https: false
-  })
-);
-}
 
 export default config;
