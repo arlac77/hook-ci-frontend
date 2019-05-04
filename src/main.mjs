abaaf7aec271911f0c4cc4e08873b51a647a161b
Markus Felten <Markus.Felten@gmx.de>
2019-05-04 20:09:20 +0200

    refactor: start using main.mjs

--- src/main.mjs
+++ src/main.mjs
@@ -1,1 +1,13 @@
-console.log('hello 2');
+
+
+export async function refresh(app) {
+  try {
+    const data = await fetch("api/state");
+    const state = await data.json();
+    app.version = state.version;
+    app.queues.length = 0;
+    app.queues.push(...state.queues);
+  } catch (err) {
+    console.error(err);
+  }
+}
