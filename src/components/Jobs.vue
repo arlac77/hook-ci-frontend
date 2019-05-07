f6097e436b84c963cc6e622ebbdc138ac72cce34
Markus Felten <Markus.Felten@gmx.de>
2019-05-08 00:16:54 +0200

    fix: use b-table for jobs too

--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="view">
-    <table class="table table-striped table-bordered table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr scope="row" v-for="job in jobs">
-          <td>{{ job.id }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <b-table class="table table-striped table-bordered table-hover"
+      :busy="isBusy"
+      :items="items"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage">
+      <div slot="table-busy" class="text-center my-2">
+        <b-spinner class="align-middle"></b-spinner>
+        <strong>Loading...</strong>
+      </div>
+    </b-table>
+    <b-row>
+      <b-col md="6" class="my-1">
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="totalRows"
+      :per-page="perPage"
+      class="my-0"
+    ></b-pagination>
+  </b-col>
+</b-row>
+
   </div>
 </template>
 
@@ -19,21 +29,45 @@
 export default {
   name: "Jobs",
   props: {
-    jobs: {
+    items: {
       type: Array,
       default: []
     }
   },
   methods: {
     async refresh() {
+      this.isBusy = true;
       const data = await fetch("api/queue/request/jobs");
-      const jobs = await data.json();
-      this.jobs.length = 0;
-      this.jobs.push(...jobs);
+      const items = await data.json();
+      this.items.length = 0;
+      this.items.push(...items);
+
+      this.totalRows = this.items.length;
+
+      this.isBusy = false;
     }
   },
   mounted() {
     this.refresh();
-  }
+  },
+  data() {
+   return {
+     totalRows: 1,
+     currentPage: 1,
+     perPage: 5,
+     pageOptions: [5, 10, 15],
+     isBusy: false,
+     fields: {
+       id: {
+         label: 'Job',
+         sortable: true
+       },
+       data: {
+         label: 'Data',
+         sortable: false
+       }
+     }
+   }
+ }
 };
 </script>
