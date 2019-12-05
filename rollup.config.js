c8d50af4649f7bff555d31a2a921c10f740780f7
Markus Felten <Markus.Felten@gmx.de>
2019-12-05 16:59:56 +0100

    fix(rollup): use @rollup/plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import replace from "rollup-plugin-replace";
 import vue from "rollup-plugin-vue";
 import css from "rollup-plugin-css-only";
 import json from "rollup-plugin-json";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import nodeGlobals from "rollup-plugin-node-globals";
 import pkg, { config } from "./package.json";
